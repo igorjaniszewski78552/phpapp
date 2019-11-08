f124eceef020799743cd664d1b8fe1625fd594d7
kl2ag1 <igor.janiszewski@lo1.sandomierz.pl>
2019-11-08 09:46:08 +0100

    oikatg

--- inc/functions.php
+++ inc/functions.php
@@ -1,41 +1,37 @@
 <?php
-
 // $pages = array(
-// 	'witam' => 'Witamy',
-// 	'formularz' => 'Formularz',
-// 	'klasa' => 'Klasy'
+// 'Witam' => 'Witamy',
+// 'formularz' => 'Formularz',
+// 'klasa' => 'Klasy'
 // );
-$ret = array(); // tablica zawierajaca wyniki zapytań
-
-function get_menu($id) {
-	global $db, $ret;
- 	db_query('SELECT * FROM menu', $ret);
- 	// print_r($ret);
- 	foreach ($ret as $k => $t) {
-		echo '
+$ret=array();//tablica asocjacyjna, ktora bedzie zawierala wyniki zapytan
+function get_menu($id){
+global $db,$ret;
+db_query('SELECT * FROM menu', $ret);
+//print_r($ret);
+ foreach ($ret as $k => $t) {
+echo '
 <li class="nav-item">
     <a class="nav-link" href="?id='.$t['plik'].'">'.$t['tytul'].'</a>
 </li>
-		';
+';
+}
+}
+function get_page_title($id){
+global $ret;
+foreach ($ret as $k => $t) {
+	if ($t['plik'] == $id){
+		echo $t['tytul'];
+		return;
 	}
 }
-
-function get_page_title($id) {
-	global $ret;
-	foreach ($ret as $k => $t){
-		if($t['plik'] == $id){
-			echo $t['tytul'];
-			return;
-		}
+//tytul domuslny
+	echo 'Aplikacja PHP';
 }
-		echo 'Aplikacja PHP';
+function get_page_content($id){
+if (file_exists($id.'.html'))
+	include($id.'.html');
+else
+	include('404.html');
 }
-
-function get_page_content($id) {
-	if (file_exists($id.'.html'))
-		include($id.'.html');
-	else
-		include('404.html');
-}
-
 ?>
